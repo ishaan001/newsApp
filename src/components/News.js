4f02f7bff90750e92cb08ccabe9c062a9f7f87e7
ishaan001 <ishaanveerdadhwal@gmail.com>
2023-11-14 19:36:49 +0100

    Feature : previous and next button added

--- src/components/News.js
+++ src/components/News.js
@@ -9,12 +9,66 @@ export class News extends Component {
     this.state = {
       article: newsData.articles,
       loading: false,
+      page: 1,
     };
   }
+
+  async componentDidMount() {
+    console.log("componentDidMount");
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=2c62d05110334d73a41aa33ad0638d8a&page=1";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({ article: parseData.articles });
+  }
+
+  handlePrevClick = async () => {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2c62d05110334d73a41aa33ad0638d8a&page=${this.state.page -1}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      article: parseData.articles
+    });
+  };
+
+  handleNextClick = async () => {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2c62d05110334d73a41aa33ad0638d8a&page=${this.state.page + 1}`;
+      let data = await fetch(url);
+    let parseData = await data.json();
+    if(parseData.articles.length === 0) {
+
+    }else {
+      this.setState({
+        page: this.state.page + 1,
+        article: parseData.articles
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-3">
-        <h2>News Monekey - Top Headlines of the day</h2>
+        <h1>News Monekey - Top Headlines of the day</h1>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &laquo; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &raquo;
+          </button>
+        </div>
         <div className="row">
           {this.state.article.map((element) => {
             return (
@@ -23,6 +77,24 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            disabled={this.state.page < 1}
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &laquo; Previous
+          </button>
+          <button
+            type="button"
+            disable={this.state.article === null}
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &raquo;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,37 @@
 import React, { Component } from "react";
+import newsImg from "../images/news.jpg"
 
 export class NewsItem extends Component {
+  constructor() {
+    super();
+    this.state = {error: false };
+  }
+
+  handleImageError() {
+    this.setState({error: true });
+  }
+
   render() {
     let { newsItemDescription } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src={newsItemDescription.urlToImage}
+            src={!this.state.error ? (newsItemDescription.urlToImage ? newsItemDescription.urlToImage : newsImg) : newsImg}
+            onError={this.handleImageError.bind(this)}
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{newsItemDescription.title}</h5>
-            <p className="card-text">{newsItemDescription.description.slice(0, 100)}...</p>
+            <h5 className="card-title">{newsItemDescription.title ? newsItemDescription.title : " "}</h5>
+            <p className="card-text">{newsItemDescription.description ? newsItemDescription.description.slice(0, 100) : " "}...</p>
 
             <a
               href={newsItemDescription.url}
               target="_blank"
               rel="noreferrer"
               /*_blank is used as on clicking the link it will open new tab*/ 
-              className="btn btn-sm btn-secondary"
+              className="btn btn-sm btn-dark"
             >
               Read More
             </a>
